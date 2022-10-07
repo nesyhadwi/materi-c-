d9e0bd5eb77b9f2701aa46766b95e5d3436daeda
nesyhadwi <111961915+nesyhadwi@users.noreply.github.com>
2022-10-07 11:33:43 +0700

    Add files via upload

---
+++ function.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void say_hello (string nama){
+  cout << "Halo, "<< nama<<endl;
+  }
+  
+  void tambah (int angka1,int angka2){
+    printf(" Penjumlahan %d + %d = %d",
+            angka1 ,angka2 ,angka1 + angka2);
+            }
+            
+  int main (){
+    say_hello ("algis");
+    tambah (10,5);
+    tambah (20,10);
+    tambah (25,25);
+  }
+  
+
+
